60596312a31e10ea5eb2f7ceea97a07fd82ff45e
IDunnoRagna <afonsomnmteixeira@gmail.com>
2020-03-05 16:19:36 +0000

    Added all the scenarios except for if it doesn't know

--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -7,36 +7,33 @@ namespace ChatBot
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello Happy World! I am god, ask me anything. (Also you can write EXIT to leave");
-            while(true)
+            Console.WriteLine("Hello Happy World! I am god, ask me anything. (Also you can write EXIT to leave)");
+            bool y = true;
+            while (y == true) 
             {
                 String x = Console.ReadLine();
-                if (x == "Gay?")
+                if (x == "Who's the best bandori girl?")
                     {
-                        Console.WriteLine("Yes");
+                        Console.WriteLine("Misaki.");
+                    }
+                if (x == "What's the best bandori song?")
+                    {
+                    Console.WriteLine("Roselia cover of Charles.");
                     }
                 if (x == "Gay?")
                     {
-                    Console.WriteLine("Yes");
+                    Console.WriteLine("Yes.");
                     }
-                if (x == "Gay?")
+                if (x == "What's the worst band?")
                     {
-                    Console.WriteLine("Yes");
-                    }
-                if (x == "Gay?")
-                    {
-                    Console.WriteLine("Yes");
+                    Console.WriteLine("RAS.");
                     }
                 if (x == "EXIT")
                     {
-                    Console.WriteLine("Goodbye");
-                    break;
-                }
-                else
-                    {
-                    Console.WriteLine("Goodbye");
-                    break;
+                    Console.WriteLine("Goodbye...");
+                    y = false;
                     }
+
             }
         }
     }
