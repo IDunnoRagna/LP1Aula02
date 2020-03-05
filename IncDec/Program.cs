3156552a2f976fa9ee58ae92e64979374f38e6ae
IDunnoRagna <afonsomnmteixeira@gmail.com>
2020-03-05 14:30:03 +0000

    Created IncDec

---
+++ IncDec/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 4;
+
+                Console.WriteLine(x++);
+                Console.WriteLine(x);
+                Console.WriteLine(x--);
+                Console.WriteLine(x);
+                Console.WriteLine(--x);
+                Console.WriteLine(++x);
+                
+        }
+    }
+}
