b22a6023d613ca35ea2e0e6f2ff605047a463a3a
IDunnoRagna <afonsomnmteixeira@gmail.com>
2020-03-05 16:06:08 +0000

    Created ChatBot added the exit and the ifs for questions

---
+++ ChatBot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello Happy World! I am god, ask me anything. (Also you can write EXIT to leave");
+            while(true)
+            {
+                String x = Console.ReadLine();
+                if (x == "Gay?")
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                if (x == "Gay?")
+                    {
+                    Console.WriteLine("Yes");
+                    }
+                if (x == "Gay?")
+                    {
+                    Console.WriteLine("Yes");
+                    }
+                if (x == "Gay?")
+                    {
+                    Console.WriteLine("Yes");
+                    }
+                if (x == "EXIT")
+                    {
+                    Console.WriteLine("Goodbye");
+                    break;
+                }
+                else
+                    {
+                    Console.WriteLine("Goodbye");
+                    break;
+                    }
+            }
+        }
+    }
+}
