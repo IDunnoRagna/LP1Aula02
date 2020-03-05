40564d1ddc88b4af623b1152d1f8b58ba7a7a4be
IDunnoRagna <afonsomnmteixeira@gmail.com>
2020-03-05 15:31:13 +0000

    All max values added

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = byte.MaxValue;
+            sbyte b = sbyte.MaxValue;
+            short c = short.MaxValue;
+            ushort d = ushort.MaxValue;
+            int e = int.MaxValue;
+            uint f = uint.MaxValue;
+            long g = long.MaxValue;
+            ulong h = ulong.MaxValue;
+            char i = char.MaxValue;
+            float j = float.MaxValue; 
+            double k = double.MaxValue;
+            decimal l = decimal.MaxValue;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+            Console.WriteLine(l);
+        }
+    }
+}
