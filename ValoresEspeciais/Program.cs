ce93d1068ceb2e4cbbf731408b24c3a6e42cd4a3
IDunnoRagna <afonsomnmteixeira@gmail.com>
2020-03-05 15:37:04 +0000

    All min values, infintes and NaN added

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -18,6 +18,21 @@ namespace ValoresEspeciais
             float j = float.MaxValue; 
             double k = double.MaxValue;
             decimal l = decimal.MaxValue;
+            byte a1 = byte.MinValue;
+            sbyte b1 = sbyte.MinValue;
+            short c1 = short.MinValue;
+            ushort d1 = ushort.MinValue;
+            int e1 = int.MinValue;
+            uint f1 = uint.MinValue;
+            long g1 = long.MinValue;
+            ulong h1 = ulong.MinValue;
+            char i1 = char.MinValue;
+            float j1 = float.MinValue;
+            double k1 = double.MinValue;
+            decimal l1 = decimal.MinValue;
+            double x = double.PositiveInfinity;
+            double y = double.NegativeInfinity;
+            float z = float.NaN;
             Console.WriteLine(a);
             Console.WriteLine(b);
             Console.WriteLine(c);
@@ -30,6 +45,21 @@ namespace ValoresEspeciais
             Console.WriteLine(j);
             Console.WriteLine(k);
             Console.WriteLine(l);
+            Console.WriteLine(a1);
+            Console.WriteLine(b1);
+            Console.WriteLine(c1);
+            Console.WriteLine(d1);
+            Console.WriteLine(e1);
+            Console.WriteLine(f1);
+            Console.WriteLine(g1);
+            Console.WriteLine(h1);
+            Console.WriteLine(i1);
+            Console.WriteLine(j1);
+            Console.WriteLine(k1);
+            Console.WriteLine(l1);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
         }
     }
 }
