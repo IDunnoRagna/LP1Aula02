3a60df9c48654aa2923ba68f05a524cc246fed74
IDunnoRagna <afonsomnmteixeira@gmail.com>
2020-03-05 15:10:39 +0000

    Now writes the volume

---
+++ Cilindro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Write the height of the cylinder: ");
+            string str = Console.ReadLine();
+            float h = float.Parse(str);
+            Console.Write("Write the radius of the cylinder: ");
+            string sri = Console.ReadLine();
+            float r = float.Parse(sri);
+            double p = 3.1415926;
+            {
+                Console.Write("The volume of the cylinder is ");
+                Console.WriteLine(p * r * r * h);
+                
+            }
+        }
+    }
+}
